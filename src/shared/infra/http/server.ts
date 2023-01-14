a55193ec2f3268b26a143f37c158c13fc1af42cc
PhilipeNagro <philipe.gomes@nagro.com.br>
2023-01-14 02:57:09 -0300

    Create Master

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,31 @@
+// import "dotenv/config";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import routes from "./routes";
+// import "../database";
+
+import AppError from "../../errors/AppError";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+  console.error(err);
+
+  return response.status(500).json({
+    status: "error",
+    message: " 🔴 Internal server error",
+  });
+});
+
+app.listen(2023, () => {
+  console.log("\n 🟢 Server running in port 2023.");
+});
