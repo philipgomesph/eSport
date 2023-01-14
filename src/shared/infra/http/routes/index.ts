72cc0a993e1f2f6433ea1fb6b6077be2e1123f78
PhilipeNagro <philipe.gomes@nagro.com.br>
2023-01-14 17:44:17 -0300

    refact(index.ts): Ajuste de importação

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+// import userRouter from "@modules/users/infra/http/routes/create.routes";
+import userRouter from "../../../../modules/users/infra/http/routes/create.routes";
 
 const routes = Router();
+
+routes.use("/user", userRouter);
 
 export default routes;
